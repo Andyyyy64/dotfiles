6b696a2d3618eadd9ab6929a5a8731d114a7c3ce
andy <takuyarossi77@gmail.com>
2026-01-16 23:28:54 +0900

    delete shit

--- i3/DecBrightness.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-current_brightness1=$(xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' ')
-
-new_brightness1=$(echo "$current_brightness1 - 0.10" | bc)
-
-xrandr --output eDP --brightness $new_brightness1
