c609b61eefbb70432d33d3e7b17f3d962288ff79
andy <takuyarossi77@gmail.com>
2026-01-17 00:01:20 +0900

    unti

--- nix/modules/home/programs/vscode/default.nix
+++ nix/modules/home/programs/vscode/default.nix
@@ -6,23 +6,23 @@
     # List of extensions
     # Note: Cursor-specific extensions or some extensions may not be available in Nixpkgs
     extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      dracula-theme.theme-dracula
-      tuttieee.emacs-mcx
-      vscodevim.vim
-      ms-ceintl.vscode-language-pack-ja
-      ms-python.python
-      ms-python.debugpy
-      ms-azuretools.vscode-docker
-      ms-vscode.cpptools
-      golang.go
-      eamodio.gitlens
-      editorconfig.editorconfig
-      hashicorp.terraform
-      redhat.vscode-yaml
-      ms-toolsai.jupyter
-      bradlc.vscode-tailwindcss
-      davidanson.vscode-markdownlint
+      jnoortheen.nix-ide                  # Nix IDE
+      dracula-theme.theme-dracula         # Dracula Theme
+      tuttieee.emacs-mcx                  # Awesome Emacs Keymap
+      vscodevim.vim                       # Vim emulation
+      ms-ceintl.vscode-language-pack-ja   # Japanese Language Pack
+      ms-python.python                    # Python support
+      ms-python.debugpy                   # Python debugger
+      ms-azuretools.vscode-docker         # Docker support
+      ms-vscode.cpptools                  # C++ support
+      golang.go                           # Go support
+      eamodio.gitlens                     # Git insights
+      editorconfig.editorconfig           # EditorConfig support
+      hashicorp.terraform                 # Terraform support
+      redhat.vscode-yaml                  # YAML support
+      ms-toolsai.jupyter                  # Jupyter notebooks
+      bradlc.vscode-tailwindcss           # Tailwind CSS support
+      davidanson.vscode-markdownlint       # Markdown linting
     ];
     userSettings = {
       "editor.fontSize" = 14;
